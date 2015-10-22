2e3f2dd6124a0844cdca04e7c4cef2d0502511f3
xululu <xuyanlu0424@163.com>
2015-10-22 14:52:27 +0800

    Added a file

---
+++ scripts/tools/weekEnToZh.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * 星期英转汉
+ */
+function weekEnToZh ($stamptime) {
+    $week_en = array('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'); 
+    $week_zh = array('周一', '周二', '周三', '周四', '周五', '周六', '周日'); 
+    return str_replace($week_en, $week_zh, date('Y年m月d日 l H:i', (int) $stamptime)); 
+}
+
+
