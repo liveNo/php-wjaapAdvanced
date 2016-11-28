3dd4fadc108d1905ebacdd076b151630fba7fbba
xululu <xuyanlu0424@163.com>
2016-11-28 12:27:41 +0800

    add ignore

---
+++ vueDemo/vueTest/src/components/Login.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="login">
+        <h1>{{ message }}</h1>
+    </div>
+</template>
+
+
+<script>
+  export default {
+    name: 'login',
+    data () {
+      return {
+        message: 'Welcome to Your Vue.js login login login'
+      }
+    }
+  }
+</script>
---
+++ vueDemo/vueTest/src/main.js
@@ -0,0 +1,1 @@
+import Vue from 'vue'import App from './App'import Home from './components/Home.vue'import VueRouter from 'vue-router'import VueResource from 'vue-resource'// 注册这两个插件Vue.use(VueResource)Vue.use(VueRouter)const router = new VueRouter()// 路由maprouter.map({  '/Home': {    component: Home  }})router.redirect({  '*': '/Home'})router.start(App, '#app')
